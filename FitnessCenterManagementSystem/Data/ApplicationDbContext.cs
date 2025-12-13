5f8b198e0a0aaa825120f53d5d45fea4e4e672c3
Alviola Permatasari <alviola.permatasari@gmail.com>
2025-12-14 00:10:04 +0300

    modified dbcontext

--- FitnessCenterManagementSystem/Data/ApplicationDbContext.cs
+++ FitnessCenterManagementSystem/Data/ApplicationDbContext.cs
@@ -1,13 +1,46 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using FitnessCenterManagementSystem.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace FitnessCenterManagementSystem.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+
+        public DbSet<FitnessCenter> FitnessCenters { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Trainer> Trainers { get; set; }
+        public DbSet<TrainerAvailability> TrainerAvailabilities { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Appointment>()
+                .HasOne(a => a.Trainer)
+                .WithMany(t => t.Appointments)
+                .HasForeignKey(a => a.TrainerId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Appointment>()
+                .HasOne(a => a.Service)
+                .WithMany()
+                .HasForeignKey(a => a.ServiceId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Appointment>()
+                .HasOne(a => a.User)
+                .WithMany()
+                .HasForeignKey(a => a.UserId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+
     }
+
+
 }
