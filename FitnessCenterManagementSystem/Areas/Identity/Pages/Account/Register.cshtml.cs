0472015cc59cd6d2b57e128d3c9158b01d78bf83
Alviola Permatasari <alviola.permatasari@gmail.com>
2025-12-16 00:06:36 +0300

    modified register.cshtml.cs

--- FitnessCenterManagementSystem/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ FitnessCenterManagementSystem/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -71,29 +71,30 @@ namespace FitnessCenterManagementSystem.Areas.Identity.Pages.Account
         /// </summary>
         public class InputModel
         {
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
+            [Required]
+            [MaxLength(50)]
+            [Display(Name = "Name")]
+            public string Name { get; set; }
+            
+            [Required]
+            [MaxLength(50)]
+            [Display(Name = "Surname")]
+            public string Surname { get; set; }
+
+
             [Required]
             [EmailAddress]
             [Display(Name = "Email")]
             public string Email { get; set; }
 
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
+            
             [Required]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             [DataType(DataType.Password)]
             [Display(Name = "Password")]
             public string Password { get; set; }
 
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
+            
             [DataType(DataType.Password)]
             [Display(Name = "Confirm password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
@@ -114,7 +115,9 @@ namespace FitnessCenterManagementSystem.Areas.Identity.Pages.Account
             if (ModelState.IsValid)
             {
                 var user = CreateUser();
-
+                
+                user.Name = Input.Name;
+                user.Surname = Input.Surname;
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
                 var result = await _userManager.CreateAsync(user, Input.Password);
