2b10783de81e1c4fed03291f24fccd601fedbae4
Alviola Permatasari <alviola.permatasari@gmail.com>
2025-12-17 01:09:04 +0300

    new controllers added

---
+++ FitnessCenterManagementSystem/Controllers/AppointmentsController.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FitnessCenterManagementSystem.Data;
+using FitnessCenterManagementSystem.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace FitnessCenterManagementSystem.Controllers
+{
+    [Authorize]
+
+    public class AppointmentsController : Controller
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public AppointmentsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Appointments
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Appointments.Include(a => a.Service).Include(a => a.Trainer).Include(a => a.User);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Appointments/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var appointment = await _context.Appointments
+                .Include(a => a.Service)
+                .Include(a => a.Trainer)
+                .Include(a => a.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (appointment == null)
+            {
+                return NotFound();
+            }
+
+            return View(appointment);
+        }
+
+        // GET: Appointments/Create
+        public IActionResult Create()
+        {
+            ViewData["ServiceId"] = new SelectList(_context.Services, "Id", "ServiceName");
+            ViewData["TrainerId"] = new SelectList(_context.Trainers, "Id", "Name");
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id");
+            return View();
+        }
+
+        // POST: Appointments/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,TrainerId,UserId,ServiceId,StartAt,EndAt,IsConfirmed,Price")] Appointment appointment)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(appointment);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ServiceId"] = new SelectList(_context.Services, "Id", "ServiceName", appointment.ServiceId);
+            ViewData["TrainerId"] = new SelectList(_context.Trainers, "Id", "Name", appointment.TrainerId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", appointment.UserId);
+            return View(appointment);
+        }
+
+        // GET: Appointments/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var appointment = await _context.Appointments.FindAsync(id);
+            if (appointment == null)
+            {
+                return NotFound();
+            }
+            ViewData["ServiceId"] = new SelectList(_context.Services, "Id", "ServiceName", appointment.ServiceId);
+            ViewData["TrainerId"] = new SelectList(_context.Trainers, "Id", "Name", appointment.TrainerId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", appointment.UserId);
+            return View(appointment);
+        }
+
+        // POST: Appointments/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,TrainerId,UserId,ServiceId,StartAt,EndAt,IsConfirmed,Price")] Appointment appointment)
+        {
+            if (id != appointment.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(appointment);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AppointmentExists(appointment.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ServiceId"] = new SelectList(_context.Services, "Id", "ServiceName", appointment.ServiceId);
+            ViewData["TrainerId"] = new SelectList(_context.Trainers, "Id", "Name", appointment.TrainerId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", appointment.UserId);
+            return View(appointment);
+        }
+
+        // GET: Appointments/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var appointment = await _context.Appointments
+                .Include(a => a.Service)
+                .Include(a => a.Trainer)
+                .Include(a => a.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (appointment == null)
+            {
+                return NotFound();
+            }
+
+            return View(appointment);
+        }
+
+        // POST: Appointments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var appointment = await _context.Appointments.FindAsync(id);
+            if (appointment != null)
+            {
+                _context.Appointments.Remove(appointment);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AppointmentExists(int id)
+        {
+            return _context.Appointments.Any(e => e.Id == id);
+        }
+    }
+}
