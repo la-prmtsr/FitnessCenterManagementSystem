dc8ec654d2505b226ea2c50739928bd1ffbe6a34
Alviola Permatasari <alviola.permatasari@gmail.com>
2025-12-21 10:26:30 +0300

    added some changes on css and .cshtml pages

--- FitnessCenterManagementSystem/Controllers/AiController.cs
+++ FitnessCenterManagementSystem/Controllers/AiController.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Net.Http.Headers;
 using System.Text;
 
 namespace FitnessCenterManagementSystem.Controllers
 {
+    
     public class AiController : Controller
     {
         
